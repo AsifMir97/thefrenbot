03332bba984d67b8b0a1125607e64616cfc1ac6c
AsifMir97 <102671069+AsifMir97@users.noreply.github.com>
2023-05-10 07:25:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telegram
+import time
 from telegram.ext import CommandHandler, Updater
 
 # Replace this with your contract address
@@ -11,13 +12,30 @@ def ca(update, context):
     message += f"Here is the link to the contract on DEXTools: {LINK}"
     update.message.reply_text(message)
 
+
+def website(update, context):
+    message = f"Here is the website  : https://fren.finance/"
+    update.message.reply_text(message)
+
+def engage_users(update, context):
+    caption = "Hey everyone, just wanted to remind you to keep Shilling $FREN! 🚀💎🙌"
+    with open("/frencool.jpg", "rb") as image:
+        context.bot.send_photo(photo=image, caption=caption)
+
+
 def main():
     # Replace this with your API token
     updater = Updater("6088145529:AAEYeB04w21w81b4oGxXQNnYGzi6FragQfo", use_context=True)
 
+#command handlers 
     updater.dispatcher.add_handler(CommandHandler("ca", ca))
+    updater.dispatcher.add_handler(CommandHandler("website", website))
 
     updater.start_polling()
+# Schedule the engagement function to run every 15 minutes
+    job_queue = updater.job_queue
+    job_queue.run_repeating(engage_users, interval=900, first=0)
+# Keep the bot running until stopped manually
     updater.idle()
 
 if __name__ == '__main__':
